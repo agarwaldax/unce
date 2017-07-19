a2b2f945607ea47c3a49183079ba784f74442aac
Daxit Agarwal <agarwaldax@gmail.com>
2017-07-19 01:05:41 -0400

    Many more desktop fixes including home video and contacts

--- js/nav.js
+++ js/nav.js
@@ -1,49 +1,26 @@
 $(document).ready(function(){
-  $(".togglemenubutton").click(function(){
+  $(".togglemenubutton, .dimpage").click(function(){
     $("nav").toggleClass("toggle");
-    $(".dimpage").fadeToggle(800);
-  });
-  
-  $(".dimpage").click(function(){
-    $("nav").toggleClass("toggle");
-    $(".dimpage").fadeToggle(800);
+    $(".dimpage").fadeToggle(500);
   });
   
   var onPage = document.getElementsByTagName("nav")[0].dataset.onpage;
+  var parent = onPage.indexOf("-sub") < 0 ? "" : "../";
   
   $(window).on("scroll", function(){
-    var sec = $("section.mobNavBar").offset().top;
-    if (onPage.indexOf("-sub") !== -1) {
-      if(sec > 50) {
-        $("section.mobNavBar").css("background", "#fff");
-        $("section.mobNavBar").css("box-shadow", "0px 0px 6px 4px rgba(0, 0, 0, 0.35)");
-        $("nav a").addClass("color");
-        $("section.mobNavBar .logo").attr("src", "../res/logo-color.svg");
-        $("section.mobNavBar .menu").attr("src", "../res/menu-color.svg");
-        
-      }
-      else {
-        $("section.mobNavBar").css("background", "none");
-        $("section.mobNavBar").css("box-shadow", "none");
-        $("nav a").removeClass("color");
-        $("section.mobNavBar .logo").attr("src", "../res/logo-white.svg");
-        $("section.mobNavBar .menu").attr("src", "../res/menu-white.svg");
-      }
+    var location = $("section.mobNavBar").offset().top - $("section.content").offset().top;
+    if (location > 50) {
+      $("section.mobNavBar").css("background", "#fff");
+      $("section.mobNavBar").css("box-shadow", "0px 0px 6px 4px rgba(0, 0, 0, 0.35)");
+      $("nav a").addClass("color");
+      $("section.mobNavBar .logo").attr("src", parent + "res/logo-color.svg");
+      $("section.mobNavBar .menu").attr("src", parent + "res/menu-color.svg");
     } else {
-      if(sec > 50) {
-        $("section.mobNavBar").css("background", "#fff");
-        $("section.mobNavBar").css("box-shadow", "0px 0px 6px 4px rgba(0, 0, 0, 0.35)");
-        $("nav a").addClass("color");
-        $("section.mobNavBar .logo").attr("src", "res/logo-color.svg");
-        $("section.mobNavBar .menu").attr("src", "res/menu-color.svg");
-      }
-      else {
-        $("section.mobNavBar").css("background", "none");
-        $("section.mobNavBar").css("box-shadow", "none");
-        $("nav a").removeClass("color");
-        $("section.mobNavBar .logo").attr("src", "res/logo-white.svg");
-        $("section.mobNavBar .menu").attr("src", "res/menu-white.svg");
-      }
+      $("section.mobNavBar").css("background", "none");
+      $("section.mobNavBar").css("box-shadow", "none");
+      $("nav a").removeClass("color");
+      $("section.mobNavBar .logo").attr("src", parent + "res/logo-white.svg");
+      $("section.mobNavBar .menu").attr("src", parent + "res/menu-white.svg");
     }
   });
 
